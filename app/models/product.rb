0b20f272b3ed03fa6917c2fd482a3e3106ccdeee
daniel <borrerodaniel89@gmail.com>
2021-01-16 15:00:05 -0300

    add gem devise and generate user model with its dependencies

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    belongs_to :user
     has_many :comment, dependent: :destroy
     validates :name, presence: true, length: { minimum: 5 }
     validates :description, presence: true
