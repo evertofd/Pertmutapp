b8a1ab629ee8694f3280a204529a2ddf2c1111ea
Matías <matias.mullerg@mail.udp.cl>
2021-01-16 16:42:25 -0300

    active storage added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+    has_one_attached :picture
     has_many :comment, dependent: :destroy
     validates :name, presence: true, length: { minimum: 5 }
     validates :description, presence: true
